c9fa91f1147da4173b78906448ba6412354fbb5d
Harsh Pareshbhai Gosaliya <153543231+harshpareshbhaigosalya@users.noreply.github.com>
2025-10-02 15:39:35 +0530

    Update binary-search.py

--- search/binary-search.py
+++ search/binary-search.py
@@ -29,16 +29,3 @@ def binary_search(nums, target):
     return -1
 
 
-# -------------------------------
-# ✅ Test Cases
-# -------------------------------
-if __name__ == "__main__":
-    # Test Case 1
-    nums1 = [1, 3, 5, 7, 9]
-    target1 = 7
-    print(f"Input: nums = {nums1}, target = {target1}\nOutput: {binary_search(nums1, target1)}\n")
-
-    # Test Case 2
-    nums2 = [2, 4, 6, 8, 10]
-    target2 = 5
-    print(f"Input: nums = {nums2}, target = {target2}\nOutput: {binary_search(nums2, target2)}")
